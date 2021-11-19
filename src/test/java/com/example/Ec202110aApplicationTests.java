49ced38fa5b91aa6974fdb7026b47b9e2f5d1cc5
furumotomasayuki <masayuki.furumoto@rakus-partners.co.jp>
2021-11-19 16:39:38 +0900

    first commit

---
+++ src/test/java/com/example/Ec202110aApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec202110aApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
