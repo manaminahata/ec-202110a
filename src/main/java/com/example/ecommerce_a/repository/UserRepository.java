900cef181ed9b20c7e14adc6517ac6089bf47118
NahataManami <manami.nahata@rakus-partners.co.jp>
2021-11-22 15:45:25 +0900

    UserRepositoryにfindByEmailAndPasswordメソッドの追加。メールアドレスとパスワードが一致するオブジェクトを取得する。

--- src/main/java/com/example/ecommerce_a/repository/UserRepository.java
+++ src/main/java/com/example/ecommerce_a/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.example.ecommerce_a.repository;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import com.example.ecommerce_a.domain.User;
@@ -18,5 +22,22 @@ public class UserRepository {
 			new BeanPropertyRowMapper<>(User.class);
 	
 	private final String TABLE_NAME = "users";
+	
+	/**
+	 * ログイン時にメールアドレスとパスワードが一致しているか確認するためのメソッド
+	 * @param email
+	 * @param password
+	 * @return　メールアドレスとパスワードが一致した施設を取得する
+	 */
+	public User findByEmailAndPassword(User user) {
+		String sql = "SELECT * FROM users WHERE email=:email AND password=:password";
+		SqlParameterSource param = new BeanPropertySqlParameterSource(user);
+		
+		List<User> userList = template.query(sql, param, USER_ROW_MAPPER);
+		if (userList.size() == 0) {
+			return null;
+		}
+		return userList.get(0);
+	}
 	
 }
