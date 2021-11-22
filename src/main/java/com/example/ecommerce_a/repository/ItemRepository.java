59c26985bac0ab30e419a13342905730a58948c6
furumotomasayuki <masayuki.furumoto@rakus-partners.co.jp>
2021-11-22 14:34:17 +0900

    ドメインクラスとレポジトリクラスの作成 以下クラスを作成しました。 User.java UserRepository.java Item.java ItemRepository.java

---
+++ src/main/java/com/example/ecommerce_a/repository/ItemRepository.java
@@ -0,0 +1,22 @@
+package com.example.ecommerce_a.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.example.ecommerce_a.domain.Item;
+
+@Repository
+public class ItemRepository {
+
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+	
+	private final RowMapper<Item> USER_ROW_MAPPER =
+			new BeanPropertyRowMapper<>(Item.class);
+	
+	private final String TABLE_NAME = "items";
+	
+}
